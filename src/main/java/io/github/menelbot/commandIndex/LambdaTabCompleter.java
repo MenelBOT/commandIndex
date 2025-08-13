8da14a6c415fe698b7d48df5bb027b954b427799
MenelBOT <77665467+MenelBOT@users.noreply.github.com>
2025-08-13 20:42:07 +0200

    Add copyright header to all source files

--- src/main/java/io/github/menelbot/commandIndex/LambdaTabCompleter.java
+++ src/main/java/io/github/menelbot/commandIndex/LambdaTabCompleter.java
@@ -1,3 +1,17 @@
+/*
+ * SPDX-License-Identifier: MPNRL-1.0
+ *
+ * Copyright (c) 2025 Menel
+ *
+ * This file is part of the CommandIndex library.
+ *
+ * Licensed under the Menel Permissive Non-Resale License, Version 1.0.
+ * You may use, modify, and distribute this software freely, but you may not
+ * sell or license it for a fee, except as a dependency of a larger work that
+ * substantially extends its functionality.
+ *
+ * Full license text: https://github.com/MenelBOT/commandIndex/blob/main/LICENSE
+ */
 package io.github.menelbot.commandIndex;
 
 
